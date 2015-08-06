79a749a0df170bd064527bdc74b89a88066ddbb8
btspm10 <btspm10@gmail.com>
2015-08-06 14:30:34 -0500

    First Upload

---
+++ app/models/pin.rb
@@ -0,0 +1,10 @@
+class Pin < ActiveRecord::Base
+
+  belongs_to :user
+
+
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+
+
+end
